667e3c51018f297d64dc934956584cde88e235d8
infuriating <luca@infs.world>
2024-02-09 08:46:59 +0100

    Added PageTransitionEffect component

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "./Header";
+import PageTransitionEffect from "./PageTransitionEffect";
 
 export default function Page({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <Header />
-      <div className="py-4 px-6">{children}</div>
+      <PageTransitionEffect>
+        <div className="py-4 px-6">{children}</div>
+      </PageTransitionEffect>
     </div>
   );
 }
