a68034ba2f1040c881466124e31327a93feb892d
infuriating <luca@infs.world>
2024-02-09 19:57:33 +0100

    added currentUser to the Page component

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Header from "./Header";
 import PageTransitionEffect from "./PageTransitionEffect";
+import { currentUser } from "@clerk/nextjs";
 
-export default function Page({ children }: { children: React.ReactNode }) {
+export default async function Page({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = await currentUser();
+  const role = user?.publicMetadata.role;
+
   return (
     <div>
-      <Header />
+      <Header
+        isSignedIn={user ? true : false}
+        imageUrl={user?.imageUrl || ""}
+        role={role}
+      />
       <PageTransitionEffect>
         <div className="py-4 px-6">{children}</div>
       </PageTransitionEffect>
