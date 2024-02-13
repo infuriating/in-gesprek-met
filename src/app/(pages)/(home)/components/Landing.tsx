488c03f85b3ff6122ea6d5235d3e3fb6344a4ea8
infuriating <luca@infs.world>
2024-02-13 16:14:59 +0100

    Added the active statement to the landing page

---
+++ src/app/(pages)/(home)/components/Landing.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import { Preloaded, usePreloadedQuery } from "convex/react";
+import React from "react";
+import { api } from "../../../../../convex/_generated/api";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Tabel from "@/components/Tabel";
+import { ArrowRightSquareIcon } from "lucide-react";
+import { useUser } from "@clerk/nextjs";
+import { motion } from "framer-motion";
+import { getTimeAgo } from "@/lib/functions";
+
+export default function Landing(props: {
+  preloadedStellingen: Preloaded<typeof api.stelling.getLatestThree>;
+  actieveStelling: Preloaded<typeof api.actieveStelling.getActieveStelling>;
+}) {
+  const stellingen = usePreloadedQuery(props.preloadedStellingen);
+  const stelling = usePreloadedQuery(props.actieveStelling);
+  const { user } = useUser();
+  if (!stellingen) return <></>;
+
+  const huidigeStelling = stellingen.filter(
+    (stellingen) => stelling && stellingen.slug === stelling.stellingSlug
+  );
+
+  return (
+    <div>
+      <Link className="px-4" href="/stellingen">
+        <Button className="flex gap-x-2 text-lg" variant={"link"}>
+          <ArrowRightSquareIcon className="h-10 w-10" />
+          <p className="text-secondary-foreground">Bekijk alle stellingen</p>
+        </Button>
+      </Link>
+      {huidigeStelling && (
+        <Card>
+          <CardHeader>
+            <CardTitle>{huidigeStelling[0].stelling}</CardTitle>
+            <CardDescription>
+              gemaakt door {huidigeStelling[0].door}
+            </CardDescription>
+            <CardContent>
+              <Tabel stelling={huidigeStelling[0]} />
+            </CardContent>
+            <CardFooter>
+              {user ? (
+                <Link href={`/stellingen/${huidigeStelling[0].slug}`}>
+                  <Button>Bekijk deze stelling</Button>
+                </Link>
+              ) : (
+                <Button variant={"outline"} disabled>
+                  Log in om te stemmen
+                </Button>
+              )}
+            </CardFooter>
+          </CardHeader>
+        </Card>
+      )}
+      <p className="py-1 font-medium text-muted-foreground">
+        De laatste drie stellingen
+      </p>
+      <div className="grid lg:grid-cols-3 gap-x-6 gap-y-4">
+        {stellingen.map((stelling, i) => (
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: i * 0.15,
+              damping: 10,
+              duration: 0.45,
+              type: "spring",
+            }}
+            key={stelling._id}
+          >
+            <Card>
+              <CardHeader>
+                <CardTitle>{stelling.stelling}</CardTitle>
+                <CardDescription>
+                  gemaakt door
+                  <span className="text-secondary-foreground font-medium">
+                    {" "}
+                    {stelling.door}
+                    <br />
+                  </span>
+                  <span className="text-xs pt-0.5">
+                    {getTimeAgo(stelling._creationTime)}
+                  </span>
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="pl-2">
+                <Tabel stelling={stelling} height={200} />
+              </CardContent>
+              <CardFooter>
+                {user ? (
+                  <Link href={`/stellingen/${stelling.slug}`}>
+                    <Button>Bekijk deze stelling</Button>
+                  </Link>
+                ) : (
+                  <Button variant={"outline"} disabled>
+                    Log in om te stemmen
+                  </Button>
+                )}
+              </CardFooter>
+            </Card>
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
+}
