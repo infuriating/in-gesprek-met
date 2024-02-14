12368d72cb78837524cff7647d9317055b5dbdd2
infuriating <luca@infs.world>
2024-02-14 12:12:45 +0100

    Refactor page component names

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -2,7 +2,7 @@ import { preloadQuery } from "convex/nextjs";
 import { api } from "../../../../convex/_generated/api";
 import Landing from "./components/Landing";
 
-export default async function ArtistsWrapper() {
+export default async function Wrapper() {
   const preloadedStellingen = await preloadQuery(api.stelling.getLatestThree);
   const actieveStelling = await preloadQuery(
     api.actieveStelling.getActieveStelling
