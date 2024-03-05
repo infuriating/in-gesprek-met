d08f981f8ce1ce26224e1c72d75ea4c573008e2e
infuriating <luca@infs.world>
2024-03-05 14:53:54 +0100

    Remove ThemeProvider import as it doesn't exist

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import ConvexClientProvider from "@/components/ConvexClientProvider";
-import { ThemeProvider } from "@/components/theme-provider";
 import Page from "@/components/Page";
 import { Toaster } from "@/components/ui/sonner";
 
