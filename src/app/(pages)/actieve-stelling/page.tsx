e2a4cc05076be3874bae97c5c1b6937a046add1d
infuriating <luca@infs.world>
2024-03-12 10:48:38 +0100

    removed user commented code

--- src/app/(pages)/actieve-stelling/page.tsx
+++ src/app/(pages)/actieve-stelling/page.tsx
@@ -1,9 +1,6 @@
 import { preloadQuery } from "convex/nextjs";
 import { api } from "../../../../convex/_generated/api";
 import ActieveStelling from "./components/ActieveStelling";
-import { currentUser } from "@clerk/nextjs/server";
-import { SignInButton, SignUpButton } from "@clerk/nextjs";
-import { Button } from "@/components/ui/button";
 
 export default async function Wrapper() {
   const preloadedStellingen = await preloadQuery(api.stelling.getAll);
@@ -11,34 +8,10 @@ export default async function Wrapper() {
     api.actieveStelling.getActieveStelling
   );
 
-  // const user = await currentUser();
-  // if (!user) {
-  //   return (
-  //     <div className="flex flex-col items-center gap-y-1">
-  //       <h1 className="text-4xl font-bold">Je bent niet ingelogd</h1>
-  //       <p className="text-muted-foreground">
-  //         Log in om op de actieve stelling te stemmen
-  //       </p>
-  //       <div className="mt-2">
-  //         <SignInButton>
-  //           <Button className="w-full">Log in</Button>
-  //         </SignInButton>
-  //         <SignUpButton>
-  //           <Button variant={"link"} className="text-white w-full flex gap-x-2">
-  //             <span className="text-muted-foreground">Nog geen account?</span>
-  //             Maak hier een account aan
-  //           </Button>
-  //         </SignUpButton>
-  //       </div>
-  //     </div>
-  //   );
-  // }
-
   return (
     <ActieveStelling
       preloadedStellingen={preloadedStellingen}
       actieveStelling={actieveStelling}
-      // user={JSON.parse(JSON.stringify(user))}
     />
   );
 }
