466d07f77220d26c7ba3e933dc1cd990c864a321
infuriating <luca@infs.world>
2024-03-12 10:43:13 +0100

    removed the stellingen page

--- src/app/(pages)/stellingen/loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function loading() {
-  return (
-    <div className="flex justify-center items-center p-8">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-500" />
-    </div>
-  );
-}
