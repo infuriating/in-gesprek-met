6f84b080b49edd82eecc5b322886a551d0d20bf6
infuriating <luca@infs.world>
2024-03-12 10:37:31 +0100

    Removed the auth call to action and user

--- src/app/(pages)/actieve-stelling/page.tsx
+++ src/app/(pages)/actieve-stelling/page.tsx
@@ -11,33 +11,34 @@ export default async function Wrapper() {
     api.actieveStelling.getActieveStelling
   );
 
-  const user = await currentUser();
-  if (!user) {
-    return (
-      <div className="flex flex-col items-center gap-y-1">
-        <h1 className="text-4xl font-bold">Je bent niet ingelogd</h1>
-        <p className="text-muted-foreground">
-          Log in om op de actieve stelling te stemmen
-        </p>
-        <div className="mt-2">
-          <SignInButton>
-            <Button className="w-full">Log in</Button>
-          </SignInButton>
-          <SignUpButton>
-            <Button variant={"link"} className="text-white w-full flex gap-x-2">
-              <span className="text-muted-foreground">Nog geen account?</span>
-              Maak hier een account aan
-            </Button>
-          </SignUpButton>
-        </div>
-      </div>
-    );
-  }
+  // const user = await currentUser();
+  // if (!user) {
+  //   return (
+  //     <div className="flex flex-col items-center gap-y-1">
+  //       <h1 className="text-4xl font-bold">Je bent niet ingelogd</h1>
+  //       <p className="text-muted-foreground">
+  //         Log in om op de actieve stelling te stemmen
+  //       </p>
+  //       <div className="mt-2">
+  //         <SignInButton>
+  //           <Button className="w-full">Log in</Button>
+  //         </SignInButton>
+  //         <SignUpButton>
+  //           <Button variant={"link"} className="text-white w-full flex gap-x-2">
+  //             <span className="text-muted-foreground">Nog geen account?</span>
+  //             Maak hier een account aan
+  //           </Button>
+  //         </SignUpButton>
+  //       </div>
+  //     </div>
+  //   );
+  // }
+
   return (
     <ActieveStelling
       preloadedStellingen={preloadedStellingen}
       actieveStelling={actieveStelling}
-      user={JSON.parse(JSON.stringify(user))}
+      // user={JSON.parse(JSON.stringify(user))}
     />
   );
 }
