dc2eaaa73dd4f9c335331f15401d487e69523267
infuriating <luca@infs.world>
2024-03-12 10:41:25 +0100

    removed the view-stelling page

--- src/app/(pages)/dashboard/view-stemmen/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { api } from "../../../../../convex/_generated/api";
-import { preloadQuery } from "convex/nextjs";
-import StellingList from "./components/StellingList";
-
-export default async function page() {
-  const preloadedStellingen = await preloadQuery(api.stelling.getAll);
-  return <StellingList preloadedStellingen={preloadedStellingen} />;
-}
